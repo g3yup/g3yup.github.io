2bf3c53e164bbf2c2e5913d5e28f247351e68ff3
g3yup <152830553+g3yup@users.noreply.github.com>
2023-12-06 14:16:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-Btn")
+const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
 	question.innerHTML = "Aaaaa, i like you too";
